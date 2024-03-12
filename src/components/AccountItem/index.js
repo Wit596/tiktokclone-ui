d462c459547ef1c628cf1a3534111e584208a10d
Wit596 <thanhdat2004.cr@gmail.com>
2024-03-12 18:31:12 +0700

    CSSForSearchResult

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-euttp/da866f32a1ed8952fa14325a7726a7a0~c5_100x100.jpeg?lk3s=a5d48078&x-expires=1710378000&x-signature=EiQ%2FQbesoeO908q%2BPffhJ06tfvc%3D"
+                alt="Name"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
