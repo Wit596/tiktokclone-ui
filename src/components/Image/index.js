ef6df566e9411a6444ee0c3073034b2ed2e84dcf
Wit596 <thanhdat2004.cr@gmail.com>
2024-03-22 11:20:45 +0700

    Optimize

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,7 +1,1 @@
-import { forwardRef } from 'react';
-
-const Image = forwardRef(({ ...props }, ref) => {
-    return <img ref={ref} {...props} />;
-});
-
-export default Image;
+export { default } from './Image'
